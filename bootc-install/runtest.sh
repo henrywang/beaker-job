08a0996fc52396e5616c83b4fab65632764711c2
Xiaofeng Wang <henrywangxf@me.com>
2024-08-06 17:42:10 +0800

    copy repo file to image
    Signed-off-by: Xiaofeng Wang <henrywangxf@me.com>

--- bootc-install/runtest.sh
+++ bootc-install/runtest.sh
@@ -17,22 +17,25 @@ else
     TIER1_IMAGE_URL="${TIER1_IMAGE_URL:-quay.io/fedora/fedora-bootc:40}"
     LOCAL_IMAGE_NAME="bootc-image:beaker"
 
-    # Should upgrade kernel in Containerfile
-    tee "$CONTAINERFILE" > /dev/null << EOF
-FROM "$TIER1_IMAGE_URL"
-ADD http://lab-02.rhts.eng.rdu.redhat.com/beaker/anamon3 /usr/local/sbin/anamon
-RUN chmod 755 /usr/local/sbin/anamon && \
-    tee /etc/yum.repos.d/beaker-harness.repo > /dev/null << REPOEOF
+    tee "${TEMPDIR}/beaker-harness.repo" > /dev/null << EOF
 [beaker-harness]
 name=beaker-harness
 baseurl=http://beaker.engineering.redhat.com/harness/Fedora40/
 enabled=1
 gpgcheck=0
-REPOEOF
-RUN dnf install -y restraint && dnf -y clean all
 EOF
 
-    podman build --tls-verify=false --retry=5 --retry-delay=10 -t "$LOCAL_IMAGE_NAME" -f "$CONTAINERFILE" .
+    # Should upgrade kernel in Containerfile
+    tee "$CONTAINERFILE" > /dev/null << EOF
+FROM "$TIER1_IMAGE_URL"
+ADD http://lab-02.rhts.eng.rdu.redhat.com/beaker/anamon3 /usr/local/sbin/anamon
+COPY beaker-harness.repo /etc/yum.repos.d/
+RUN chmod 755 /usr/local/sbin/anamon && \
+    dnf install -y restraint && \
+    dnf -y clean all
+EOF
+
+    podman build --tls-verify=false --retry=5 --retry-delay=10 -t "$LOCAL_IMAGE_NAME" -f "$CONTAINERFILE" "$TEMPDIR"
 
     podman run \
         --rm \
