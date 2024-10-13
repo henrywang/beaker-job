f7b6752b56f3bfff882a99995088c9e7308b9ad7
Xiaofeng Wang <henrywangxf@me.com>
2024-10-13 23:23:30 +0800

    fix root login ssh key issue
    Signed-off-by: Xiaofeng Wang <henrywangxf@me.com>

--- bootc-install/runtest.sh
+++ bootc-install/runtest.sh
@@ -10,7 +10,9 @@ if [[ "$ROOT_FSTYPE" == "overlay" ]]; then
     rstrnt-report-result switch PASS 0
     exit 0
 else
-    dnf install -y podman
+    # replace with your public key
+    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtJv3QKdqQ+0+jJND7bXVq9ux87yyi4qyJk7iOsX2VsgAUuYXpBf337p5yNB3N1kjOwGYSDjvDvS7GuhdatuvJI3/xzcyodbwJp32AT76e9uvUQHTBBGmUvBLzw3nk8ZDNp5d4rt2cZvlhv7lzDSt30DF14ivg5Xp/V0tK0BEfFlvYHuHheDeiSOQRQ392J7TefPQOW+JpxANU4Bxc1aHIettaIqQMWm9r4ZELd8M83IYt5Btp1bPsnfYywQMYqNXyDuhwhcsBTR5kVObP0DwxKZbMNPmA2lBvrX2GMIa+qfvKIW87KooaoPLt7CR7/DKfQ1S492L1wIwNUPUBLsQD xiaofwan@dhcp-8-203.nay.redhat.com' > /tmp/id_rsa.pub
+
     podman run \
         --rm \
         --tls-verify=false \
@@ -19,9 +21,11 @@ else
         -v /:/target \
         -v /dev:/dev \
         -v /var/lib/containers:/var/lib/containers \
+        -v /tmp:/output \
         --security-opt label=type:unconfined_t \
         "quay.io/xiaofwan/fedora-bootc-os_replace:beaker" \
-        bootc install to-existing-root
+        bootc install to-existing-root \
+        --root-ssh-authorized-keys /output/id_rsa.pub
 
     rstrnt-reboot
     exit 0
